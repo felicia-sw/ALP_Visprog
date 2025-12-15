ed612400aaec6e68d12032e6a2aec2d8831afd51
felicia-sw <felicia.sword@gmail.com>
2025-12-15 16:37:52 +0700

    Buttons for demo

--- app/src/main/java/com/example/alp_visprog/views/HomeView.kt
+++ app/src/main/java/com/example/alp_visprog/views/HomeView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -35,7 +36,28 @@ fun HomeView(
                 text = "Welcome to the home screen",
                 style = MaterialTheme.typography.bodyLarge
             )
+            Button(
+                onClick = {
+                    // Navigate to "Reply to Post #1"
+                    navControlIer.navigate("create_exchange/1")
+                },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFEF6C35))
+            ) {
+                Text("Test: Offer Help for Post #1")
+            }
+            // --- TEMP BUTTON TO TEST FEATURE ---
+            Button(
+                onClick = {
+                    // Navigate to the route we defined in AppRouting
+                    // We hardcode "1" for testing (representing Post ID #1)
+                    navControlIer.navigate("exchange_list/1")
+                }
+            ) {
+                Text(text = "See Offers for Post #1")
+            }
+
         }
+
     }
 }
 
