f5fd1acd82bfb56b462467d13f85ae71727cab3a
Eileen <ecynthia01@student.ciputra.ac.id>
2026-01-03 23:32:27 +0700

    update spy sesuai dgn backend

--- app/src/main/java/com/example/alp_visprog/models/HelpRequestModel.kt
+++ app/src/main/java/com/example/alp_visprog/models/HelpRequestModel.kt
@@ -1,6 +1,13 @@
 package com.example.alp_visprog.models
 
 // 1. The main model (What we get from the DB)
+
+data class UserInHelpRequest(
+    val id: Int,
+    val name: String,
+    val email: String
+)
+
 data class HelpRequestModel(
     val id: Int,
     val nameOfProduct: String,
@@ -11,7 +18,7 @@ data class HelpRequestModel(
     val isCheckout: Boolean,
     val categoryId: Int,
     val userId: Int,
-    val createdAt: String
+    val createdAt: String,
     val user: UserInHelpRequest?
 )
 
