a6da5dc1d434eeebf0632b3dc128c2879dc52a4c
Eileen <ecynthia01@student.ciputra.ac.id>
2026-01-03 18:02:57 +0700

    update to match the backedn schema

--- app/src/main/java/com/example/alp_visprog/models/HelpRequestModel.kt
+++ app/src/main/java/com/example/alp_visprog/models/HelpRequestModel.kt
@@ -3,17 +3,26 @@ package com.example.alp_visprog.models
 // 1. The main model (What we get from the DB)
 data class HelpRequestModel(
     val id: Int,
-    val title: String,
+    val nameOfProduct: String,
     val description: String,
-    val category: String, // e.g., "Physical", "Academic", "Borrowing"
-    val postedDate: String
+    val exchangeProductName: String,
+    val location: String,
+    val imageUrl: String,
+    val isCheckout: Boolean,
+    val categoryId: Int,
+    val userId: Int,
+    val createdAt: String
+    val user: UserInHelpRequest?
 )
 
 // 2. The Create Request (What we send to the API)
 data class CreateHelpRequestRequest(
-    val title: String,
+    val nameOfProduct: String,
     val description: String,
-    val category: String
+    val exchangeProductName: String,
+    val location: String,
+    val imageUrl: String,
+    val categoryId: Int
 )
 
 // 3. Response wrapper (if getting a list of posts)
